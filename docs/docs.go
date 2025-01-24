763345154c5802bea8bb19105007c7c1b5d8b697
Reid Harrisson <reid.al.harrisson@gmail.com>
2025-01-24 15:58:54 +0100

    Revert "feat: add endpoint"
    This reverts commit 25f3f35eb348f9427ceb2d0436f11b3f2ca94ddb.

--- docs/docs.go
+++ docs/docs.go
@@ -14,103 +14,17 @@ const docTemplate = `{
     },
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
-    "paths": {
-        "/image": {
-            "post": {
-                "description": "Convert an image to a specified format",
-                "consumes": [
-                    "multipart/form-data",
-                    " application/json"
-                ],
-                "produces": [
-                    "multipart/form-data"
-                ],
-                "tags": [
-                    "Image"
-                ],
-                "summary": "Convert an image",
-                "parameters": [
-                    {
-                        "type": "file",
-                        "description": "Image file to convert",
-                        "name": "file",
-                        "in": "formData",
-                        "required": true
-                    },
-                    {
-                        "description": "Conversion configuration details",
-                        "name": "params",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/requests.ImageRequest"
-                        }
-                    }
-                ],
-                "responses": {
-                    "201": {
-                        "description": "Conversion successful",
-                        "schema": {
-                            "$ref": "#/definitions/responses.Message"
-                        }
-                    },
-                    "400": {
-                        "description": "Invalid request",
-                        "schema": {
-                            "$ref": "#/definitions/responses.Error"
-                        }
-                    },
-                    "500": {
-                        "description": "Internal server error",
-                        "schema": {
-                            "$ref": "#/definitions/responses.Error"
-                        }
-                    }
-                }
-            }
-        }
-    },
-    "definitions": {
-        "requests.ImageRequest": {
-            "type": "object",
-            "properties": {
-                "back_color": {
-                    "type": "string",
-                    "example": "#000000"
-                },
-                "fore_color": {
-                    "type": "string",
-                    "example": "#FFFFFF"
-                }
-            }
-        },
-        "responses.Error": {
-            "type": "object",
-            "properties": {
-                "error": {
-                    "type": "string"
-                }
-            }
-        },
-        "responses.Message": {
-            "type": "object",
-            "properties": {
-                "message": {
-                    "type": "string"
-                }
-            }
-        }
-    }
+    "paths": {}
 }`
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
+	Version:          "",
 	Host:             "",
-	BasePath:         "/api/v1/",
+	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "Image Converter API",
-	Description:      "RESTful API endpoints for Image Conversion",
+	Title:            "",
+	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	_ "image-converter/docs"
-	"image-converter/handlers"
 	"image-converter/server"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,12 +11,6 @@ import (
 func ConfigRoutes(server *server.Server) {
 	server.App.Get("/swagger/*", swagger.HandlerDefault)
 	server.App.Get("/", redirectToSwagger)
-
-	apiV1 := server.App.Group("/api/v1")
-
-	imageHandler := handlers.CreateImageHandler()
-	imageGroup := apiV1.Group("/image")
-	imageGroup.Post("", imageHandler.Convert)
 }
 
 func redirectToSwagger(context *fiber.Ctx) error {
